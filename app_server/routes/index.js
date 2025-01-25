94854bdb49a26c103880714db2bc6b3b58a85c07
Vincent Yanez <vincent.yanez@snhu.edu>
2025-01-25 18:48:16 -0500

    Updated travel routes, views, and added new image

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,4 +5,16 @@ const ctrlMain = require('../controllers/main');
 //GET home page. 
 router.get('/', ctrlMain.index) 
 
+router.get('/api/things', (req, res) => {
+    const things = [
+        { id: 1, name: 'Thing One', isActive: true },
+        { id: 2, name: 'Thing Two', isActive: false },
+        { id: 3, name: 'Thing Three', isActive: true }
+    ];
+
+    const activeThings = things.filter(thing => thing.isActive);
+    res.json(activeThings);
+});
+
 module.exports = router;
+
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -5,4 +5,10 @@ var controller = require('../controllers/travel');
 //GET travel page. 
 router.get('/', controller.travel);
 
+// GET edit page for a specific trip
+router.get('/edit/:id', controller.editTrip);
+
+// POST updated trip details
+router.post('/update/:id', controller.updateTrip);
+
 module.exports = router;
